7c7c3232cf630e17563a3a81b53e292f90677e71
Jascha Ehrenreich <jascha@jaeh.at>
2016-03-03 13:23:36 +0100

    add availableVersions http request

--- src/redux/modules/versions.js
+++ src/redux/modules/versions.js
@@ -1,6 +1,9 @@
 import { createAction, handleActions } from 'redux-actions';
 import Immutable from 'immutable';
 
+import { fetchJSON } from 'utils/http';
+import { getItem } from 'utils/storage';
+
 import { versions } from 'GLOBALS';
 
 const initialState = Immutable.Map(versions);
@@ -10,6 +13,9 @@ const initialState = Immutable.Map(versions);
 // ------------------------------------
 
 export const LOAD_CURRENT_VERSION = 'LOAD_CURRENT_VERSION';
+export const AVAILABLE_VERSIONS_REQUEST = 'AVAILABLE_VERSIONS_REQUEST';
+export const AVAILABLE_VERSIONS_SUCCESS = 'AVAILABLE_VERSIONS_SUCCESS';
+export const AVAILABLE_VERSIONS_ERROR = 'AVAILABLE_VERSIONS_ERROR';
 
 // ------------------------------------
 // Actions
@@ -18,11 +24,21 @@ export const LOAD_CURRENT_VERSION = 'LOAD_CURRENT_VERSION';
 export const loadCurrentVersion =
   createAction(
     LOAD_CURRENT_VERSION,
-    value => value
+    async value =>
+      await getItem(value)
+  );
+
+export const fetchAvailableVersions =
+  createAction(
+    AVAILABLE_VERSIONS_REQUEST,
+    async url =>
+      console.log('fetching url', url) ||
+      await fetchJSON({ url, json: true })
   );
 
 export const actions = {
   loadCurrentVersion,
+  fetchAvailableVersions,
 };
 
 // ------------------------------------
@@ -31,9 +47,19 @@ export const actions = {
 
 export default handleActions({
 
-  // a pixel has been hovered
   [LOAD_CURRENT_VERSION]:
     (state, { payload: p }) =>
       state,
+
+  [AVAILABLE_VERSIONS_REQUEST]: {
+    next(state, { payload }) {
+      console.log('next', { payload });
+      return state.set('fetchAvailableVersionsError', false);
+    },
+    throw(state, { payload }) {
+      console.log('throw', { payload });
+      return state.set('fetchAvailableVersionsError', payload.message);
+    },
+  },
 
 }, initialState);
