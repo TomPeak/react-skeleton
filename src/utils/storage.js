6ec5570950c1738e995c8d6b3faffc0e2112a8a6
Jascha Ehrenreich <jascha@jaeh.at>
2016-03-04 15:57:29 +0100

    clean up storage

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -2,10 +2,8 @@ import localforage from 'localforage';
 
 export const getItem =
   key =>
-    console.log('getItem', { key }) ||
     localforage.getItem(key);
 
 export const setItem =
   (key, value) =>
-    console.log('setItem', { key, value }) ||
-    localforage.setItem(key, value);
+    localforage.setItem (key, value);
